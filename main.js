0eba2f0a1c3198ec3e442685ae62cead3f81fa85
Hulikner <hulikner@gmail.com>
2022-02-17 13:22:22 -0600

    functions-parameters

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (value) => {
+return value/4
+
+}
+
+// Invoke the function and store the return value
+const answer = quarterValue(20)
+
+// Log the return value
+console.log(answer)
